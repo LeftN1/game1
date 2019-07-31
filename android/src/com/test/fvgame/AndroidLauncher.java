82a12b7de7c62756ddd4448088aba273c79d449c
LeftN1 <voroniukm@ukr.net>
2019-07-31 23:54:20 +0300

    Initial commit. Sprites added

---
+++ android/src/com/test/fvgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.test.fvgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.test.fvgame.FVGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FVGame(), config);
+	}
+}
---
+++ desktop/src/com/test/fvgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.test.fvgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.test.fvgame.FVGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FVGame(), config);
+	}
+}
